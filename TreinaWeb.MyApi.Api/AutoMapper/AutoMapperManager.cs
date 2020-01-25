0cda46200a3bfe1c19d23ef39cfac5ee45ee395b
wjnnora <wellington_nora94@hotmail.com>
2020-01-25 12:49:35 -0300

    feat(AutoMapper): configura o AutoMapper para trabalhar com o DTO pattern

---
+++ TreinaWeb.MyApi.Api/AutoMapper/AutoMapperManager.cs
@@ -0,0 +1,44 @@
+﻿using _0_Domain;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TreinaWeb.MyApi.Api.DTOs;
+
+namespace TreinaWeb.MyApi.Api.AutoMapper
+{
+    public class AutoMapperManager
+    {
+        private MapperConfiguration _config;
+        private static readonly Lazy<AutoMapperManager> _instance
+            = new Lazy<AutoMapperManager>(() =>
+            {
+                return new AutoMapperManager();
+            });
+
+        public static AutoMapperManager Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        public IMapper Mapper
+        {
+            get
+            {
+                return _config.CreateMapper();
+            }
+        }
+        public AutoMapperManager()
+        {
+            _config = new MapperConfiguration((cfg) =>
+            {
+                cfg.CreateMap<Aluno, AlunoDTO>();
+                cfg.CreateMap<AlunoDTO, Aluno>();
+            });
+        }
+    }
+}
